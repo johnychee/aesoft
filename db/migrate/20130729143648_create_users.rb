ca52f0ad0d9a24f0c7a06605257e076ade269d01
Johny <johny.cheerful@gmail.com>
2013-09-05 00:11:49 +0200

    changed migration to older

--- db/migrate/20130729143648_create_users.rb
+++ db/migrate/20130729143648_create_users.rb
@@ -1,9 +1,15 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+
+  def up
     create_table :users do |t|
       t.string :email
 
       t.timestamps
     end
   end
+
+  def down
+    drop_table :users
+  end
+
 end
